c3a0efe4d2e69b86c4e46273fa1312bc5a1bd104
Pablo <pilj@virket.com>
2020-05-11 18:45:26 -0500

    Tag Manager

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M9ZNP6P');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chuck Norris</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:400" rel="stylesheet">
-    <link rel="stylesheet" href="https://chuck-norris-media.s3.us-east-2.amazonaws.com/css/styles.css">
+    <link rel="stylesheet" href="css/styles.min.css">
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9ZNP6P"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <main class="main">
         <section class="section-main">
             <div class="media" >
@@ -19,7 +30,7 @@
             </div>
 
             <div class="control">
-                <input class="btn-green" type="button" id="newJoke" value="Message">
+                <a class="btn-green"  id="newJoke" >Message </a>
             </div>
 
             <audio id="audioPlayback" controls autoplay style="display:none">
